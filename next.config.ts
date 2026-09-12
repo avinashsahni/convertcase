f1d856646829bff0a96dfaf12e263e543ebc0ac5
Avinash Sahni <avinash.dgliger@vendors.ooredoo.mv>
2026-09-12 13:00:04 +0530

    config file changes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  compress: false,
-
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
